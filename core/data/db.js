8b765067e251dafd2cf28302c85314ae4b7a384a
dnfrolov <goldtool@gmail.com>
2015-08-16 14:33:12 +0300

    add connection to db as string. read value from env

--- core/data/db.js
+++ core/data/db.js
@@ -3,11 +3,9 @@
 var config = require('config');
 var Sequelize = require('sequelize');
 
-var dbConfig = config.get('db');
+var dbString = process.env.DATABASE_URL || process.env.CLEARDB_DATABASE_URL || config.get('db');
 
-module.exports = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
+module.exports = new Sequelize(dbString, {
     define: {
         freezeTableName: true
     }
